c00a67bf993971ce4d70f3cf784068d0b2c4a618
DaniilYusha <ilushenko79@gmail.com>
2020-07-24 18:22:44 +0300

    Add autoload file

---
+++ autoload.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'i18n'
+require 'codebreaker'
+
+require_relative 'services/output_service'
+require_relative 'services/registrator_service'
+
+require_relative 'modules/questioner'
+
+require_relative 'entities/console'
+require_relative 'entities/decorator'
