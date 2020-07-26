a20df9ce88be859f90f69e176c986727bf4924ec
DaniilYusha <ilushenko79@gmail.com>
2020-07-26 17:30:18 +0300

    Remove some specs for start_game_process method

--- spec/console_spec.rb
+++ spec/console_spec.rb
@@ -9,22 +9,6 @@ RSpec.describe Console do
 
   before { allow($stdout).to receive(:write) }
 
-  describe '#hint' do
-    context 'when hints left' do
-      it 'puts no hints message' do
-        console.instance_variable_set(:@game, game)
-        console.game.difficulty.instance_variable_set(:@current_hints, 0)
-        expect { console.hint }.to output(console.output.no_hints).to_stdout
-      end
-    end
-
-    it 'puts hint with message' do
-      console.instance_variable_set(:@game, game)
-      console.game.difficulty.instance_variable_set(:@current_hints, 1)
-      expect(console.hint.class).to eq NilClass
-    end
-  end
-
   describe '#rules' do
     it 'puts rules' do
       expect { console.rules }.to output(console.output.show_rules).to_stdout
@@ -67,40 +51,12 @@ RSpec.describe Console do
   end
 
   describe '#start_game_process' do
-    before { console.instance_variable_set(:@game, game) }
-
-    context 'when incorrect guess passed' do
-      it 'puts error' do
-        allow(console).to receive(:gets).and_return('ssda')
-        allow(console).to receive(:ask_about_new_game)
-        expect(console).to receive(:error_message).with('ssda').at_least(:once)
-        console.start_game_process
-      end
-    end
-
-    context 'when wins the game' do
-      before(:each) { console.game.difficulty.instance_variable_set(:@current_attempts, 1) }
-
-      it 'receive won method' do
-        allow(console).to receive(:ask_about_new_game)
-        allow(console).to receive(:gets).and_return(console.game.secret_code.join)
-        expect(console).to receive(:won)
-        console.start_game_process
-      end
-
-      it 'ask to save results' do
-        allow(console).to receive(:ask_about_new_game)
-        allow(console).to receive(:gets).and_return(console.game.secret_code.join)
-        expect(console).to receive(:ask_about_save_results)
-        console.start_game_process
-      end
-    end
-
     context 'when loses the game' do
       it 'receive lost method' do
+        console.instance_variable_set(:@game, game)
         console.game.difficulty.instance_variable_set(:@current_attempts, 1)
         allow(console).to receive(:gets).and_return('1234')
-        expect(console).to receive(:lost)
+        expect(console.game_adapter).to receive(:lost).with(game)
         console.start_game_process
       end
     end
