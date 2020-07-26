44762b6cab1c7c2bb71c4cf019318a246a0b8184
DaniilYusha <ilushenko79@gmail.com>
2020-07-26 13:55:21 +0300

    Add leave method for NO case in ask_about_new_game

--- modules/questioner.rb
+++ modules/questioner.rb
@@ -10,46 +10,46 @@ module Questioner
   HINT_COMMAND = 'hint'
 
   def ask_about_new_game
-    @output.again
+    output.again
     input = gets.chomp
     case input
-    when YES then start_new_game
-    when NO then exit
-    else @output.unexpected_command
+    when YES then game_adapter.start_new_game
+    when NO then leave
+    else output.unexpected_command
     end
     ask_about_new_game
   end
 
   def ask_about_save_results
-    @output.save
+    output.save
     input = gets.chomp
     case input
-    when YES then return @statistics.store game
+    when YES then return statistics.store game
     when NO then return
-    else @output.unexpected_command
+    else output.unexpected_command
     end
     ask_about_save_results
   end
 
   def ask_choose_game_option
-    @output.commands_description
+    output.commands_description
     input = gets.chomp
     case input
     when START_COMMAND then start
     when RULES_COMMAND then rules
     when STATS_COMMAND then stats
     when EXIT_COMMAND then leave
-    else @output.unexpected_command
+    else output.unexpected_command
     end
   end
 
   def ask_choose_command_in_game_process
-    @output.enter_guess
+    output.enter_guess
     input = gets.chomp
     case input
-    when HINT_COMMAND then hint
+    when HINT_COMMAND then game_adapter.hint
     when EXIT_COMMAND then leave
-    else check_guess(input)
+    else game_adapter.check_guess(input)
     end
   end
 end
